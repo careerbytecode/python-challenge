ab9a73e14a5c7521df44ba4a7a4869bd16a6f3ca
Pankaj Pratap SIngh <pankajsys.engg@gmail.com>
2025-07-02 21:28:08 +0400

    Pushed to production like a rebel

--- Project1-FileProcessingAutomation/csv_file_processing.py
+++ Project1-FileProcessingAutomation/csv_file_processing.py
@@ -1,6 +1,6 @@
 import csv, os, sys, json
-csv_file = 'sample.csv'
-json_file = 'sample.json'
+csv_file = "sample.csv"
+output_file = "output.csv"
 
 if not os.path.exists(csv_file):
     print(f"Error: The file {csv_file} does not exist.")
@@ -8,10 +8,9 @@ if not os.path.exists(csv_file):
 
 with open(csv_file, mode='r') as file:
     reader = csv.DictReader(file)
-    with open("output.csv", 'w') as other_file:
+    with open(output_file, 'w') as other_file:
         writer = csv.DictWriter(other_file, fieldnames=['name', 'email'])
         if other_file.tell() == 0:
             writer.writeheader()
         for row in reader:
             writer.writerow({'name': row['name'], 'email': row['email']})
-    
